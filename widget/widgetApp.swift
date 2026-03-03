260d02b1f92b6a0091ada1f36b2fbc9995711469
Karina <rurumii@users.noreply.github.com>
2026-03-03 01:16:29 +0100

    Initial Commit

---
+++ widget/widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetApp.swift
+//  widget
+//
+//  Created by Karina on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
